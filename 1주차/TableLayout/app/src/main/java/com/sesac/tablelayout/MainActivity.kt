4d870d4cfe3c3ce3ce44051de72d67fd26e9a0cc
antique123 <110442762+antique123@users.noreply.github.com>
2022-08-02 14:31:28 +0900

    TableLayout commit
    TableLayout 사용 예제 commit

---
+++ 1주차/TableLayout/app/src/main/java/com/sesac/tablelayout/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.sesac.tablelayout
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
+import androidx.core.content.ContextCompat
+import com.sesac.tablelayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setStatusBarColor()
+    }
+
+    private fun setStatusBarColor() {
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val wic = window.decorView.windowInsetsController
+            wic?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+}
